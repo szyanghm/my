a8b3bc09c759402128c0310d29d66d3733e75f0d
v_haimiyang <sz_yanghaiming@163.com>
2019-12-17 18:01:50 +0800

    22222222222222222222

---
+++ non-valent-service/src/main/java/com/non/valent/NonValentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.non.valent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonValentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonValentServiceApplication.class, args);
+	}
+
+}
---
+++ non-valent-service/src/test/java/com/non/valent/NonValentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.non.valent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonValentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
