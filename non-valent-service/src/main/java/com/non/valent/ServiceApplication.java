1d9856cc93448fbf77cb1515aefe45321247bccf
v_haimiyang <sz_yanghaiming@163.com>
2019-12-19 10:19:29 +0800

    maven去除测试打包

--- non-valent-service/src/main/java/com/non/valent/ServiceApplication.java
+++ non-valent-service/src/main/java/com/non/valent/ServiceApplication.java
@@ -2,12 +2,14 @@ package com.non.valent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class NonValentServiceApplication {
+@EnableEurekaClient
+public class ServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NonValentServiceApplication.class, args);
+		SpringApplication.run(ServiceApplication.class, args);
 	}
 
 }
--- non-valent-service/src/test/java/com/non/valent/ServiceApplicationTests.java
+++ non-valent-service/src/test/java/com/non/valent/ServiceApplicationTests.java
@@ -1,10 +1,10 @@
 package com.non.valent;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NonValentServiceApplicationTests {
+class ServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
