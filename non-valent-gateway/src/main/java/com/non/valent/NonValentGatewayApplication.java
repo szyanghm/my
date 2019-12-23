b9e48a8313a7c513344379872300a06a6d6310c0
v_haimiyang <sz_yanghaiming@163.com>
2019-12-23 18:08:16 +0800

    12.20提交更新

---
+++ non-valent-gateway/src/main/java/com/non/valent/NonValentGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.non.valent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonValentGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonValentGatewayApplication.class, args);
+	}
+
+}
