aaead2f5a6ae93caedc8f1c96d0351725eaa9b1a
v_haimiyang <sz_yanghaiming@163.com>
2019-12-25 18:09:20 +0800

    12.25提交更新

--- non-valent-service/src/main/java/com/non/valent/ServiceApplication.java
+++ non-valent-service/src/main/java/com/non/valent/ServiceApplication.java
@@ -1,11 +1,15 @@
 package com.non.valent;
 
+import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
+import com.alicp.jetcache.anno.config.EnableMethodCache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableMethodCache(basePackages = "com.non.valent")
+@EnableCreateCacheAnnotation
 public class ServiceApplication {
 
 	public static void main(String[] args) {
