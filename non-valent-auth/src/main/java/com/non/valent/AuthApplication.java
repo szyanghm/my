8542d61cff31649b735f36782adf2c6257ab2745
v_haimiyang <sz_yanghaiming@163.com>
2020-01-17 17:45:00 +0800

    2020.1.17提交更新

--- non-valent-auth/src/main/java/com/non/valent/AuthApplication.java
+++ non-valent-auth/src/main/java/com/non/valent/AuthApplication.java
@@ -3,7 +3,9 @@ package com.non.valent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class AuthApplication {
 
--- non-valent-gateway/src/main/java/com/non/valent/filter/AccessGatewayFilter.java
+++ non-valent-gateway/src/main/java/com/non/valent/filter/AccessGatewayFilter.java
@@ -3,12 +3,16 @@ package com.non.valent.filter;
 import com.non.valent.service.AuthService;
 import com.non.valent.vo.ResultVO;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
@@ -27,7 +31,21 @@ public class AccessGatewayFilter implements GlobalFilter {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
         String token = request.getHeaders().getFirst("Authorization");
-        ResultVO resultVO = authService.getJwt(token);
-        return null;
+        if(StringUtils.isNotBlank(token)){
+            ResultVO resultVO = authService.getJwt(token);
+        }
+        return chain.filter(exchange);
+    }
+
+    /**
+     * 网关拒绝，返回401
+     *
+     * @param
+     */
+    private Mono<Void> unauthorized(ServerWebExchange serverWebExchange) {
+        serverWebExchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        DataBuffer buffer = serverWebExchange.getResponse()
+                .bufferFactory().wrap(HttpStatus.UNAUTHORIZED.getReasonPhrase().getBytes());
+        return serverWebExchange.getResponse().writeWith(Flux.just(buffer));
     }
 }
